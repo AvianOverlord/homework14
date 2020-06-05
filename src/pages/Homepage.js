fcc8452e211b2781128b6a9bcc97f761d5149ff0
William Rave <wrave@gwu.edu>
2020-06-04 21:09:24 -0400

    Homepage framework

---
+++ src/pages/Homepage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import API from "../utils/API";
+
+class Homepage extends React.Component{
+    state = {
+
+    }
+
+    componentDidMount()
+    {
+        
+    }
+
+    render()
+    {
+
+    }
+}
+
+export default Homepage;
