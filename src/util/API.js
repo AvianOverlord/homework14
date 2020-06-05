b662348dd688edecc7a36df6b0d22a1dc1bd2383
William Rave <wrave@gwu.edu>
2020-06-05 18:02:46 -0400

    Changed API to gather 30 employees, not one.

--- src/util/API.js
+++ src/util/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default {
-    getRandomEmployee: function() {
-        return axios.get("https://randomuser.me/api/");
+    getRandomEmployees: function() {
+        return axios.get("https://randomuser.me/api/?results=30");
     }
 };
