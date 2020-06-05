fc5d61d697f88e47047190dd6bb9c56ec377618d
William Rave <wrave@gwu.edu>
2020-06-04 20:55:27 -0400

    Added homepage route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
+          <Route exact path="/" component = {Homepage}/>
       </Router>
     </div>
   );
